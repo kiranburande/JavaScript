53fd4bced975e2515ca93b91995834d9785172ed
Kiran Burande <kiransburande6172@gmail.com>
2025-01-24 16:54:23 +0530

    Add files via upload

---
+++ JavaScript/JS1/js1.js
@@ -0,0 +1,9 @@
+
+
+let name = "Rohit Sharma", rollNo = 1001, branch="B.Tech", address="Hyderabad", clg = "Naresh It";
+
+console.log("Name = "+name);
+console.log("Roll No = "+rollNo);
+console.log("Branch = "+branch);
+console.log("Address = "+address);
+console.log("Collage Name = "+clg);
