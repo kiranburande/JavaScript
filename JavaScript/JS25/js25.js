70d6988974da6b09b4505d027174777fce55c5bf
Kiran Burande <kiransburande6172@gmail.com>
2025-03-04 17:05:57 +0530

    Add files via upload

---
+++ JavaScript/JS25/js25.js
@@ -0,0 +1,16 @@
+let id = +prompt('Enter User id :');
+if(id==1001)
+{
+    let pass = +prompt('Enter user password :');
+    if(pass==1010)
+    {
+        console.log('Welcome Kiran...');
+    }
+    else{
+        console.log('Invalid Password...');
+    }
+
+}
+else{
+    console.log('Invalid Id');
+}
